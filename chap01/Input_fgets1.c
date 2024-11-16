b3cef9a07ecaeb252d6c78b0d750afa5a3ead178
csbyun-data <time_series@naver.com>
2024-11-16 12:18:13 +0900

    Create Input_fgets1.c

---
+++ chap01/Input_fgets1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char* ptr;
+	
+	ptr = (char*)malloc(sizeof(char)*50);
+	fputs("input String : ", stdout);  // stdout 표준촐력 stream 
+	
+	if(fgets(ptr, 50, stdin) != NULL)  // stdin 표준입력 stream 
+		fputs(ptr, stdout);            // stderr 표준에러 stream
+	free(ptr);
+	
+	return 0;
+}
