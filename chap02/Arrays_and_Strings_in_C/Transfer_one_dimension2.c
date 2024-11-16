94d6d60ddd364818d304870db5b1a16e531bd09c
csbyun-data <time_series@naver.com>
2024-11-16 23:36:52 +0900

    Create Transfer_one_dimension2.c

---
+++ chap02/Arrays_and_Strings_in_C/Transfer_one_dimension2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define ARRAY_SIZE 8
+
+void ReadArray(int acData[]) {
+  int idx = 0;
+
+  for( idx=0; idx<ARRAY_SIZE; ++idx)
+    printf("%d ", acData[idx]);
+}
+
+int main( int argc, char *argv[])
+{
+  // Create an array
+  int aiData[ARRAY_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+  //Pass array as a paramenter
+  ReadArray(aiData);
+
+  return 0;
+}
