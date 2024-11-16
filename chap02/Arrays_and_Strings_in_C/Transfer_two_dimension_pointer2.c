824064cb14a07fb6cf58cd6fc5158c7a9bb9c4ef
csbyun-data <time_series@naver.com>
2024-11-16 23:52:23 +0900

    Create Transfer_two_dimension_pointer2.c

---
+++ chap02/Arrays_and_Strings_in_C/Transfer_two_dimension_pointer2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Size if the created array
+#define M 3
+
+void ReadArray( int (*piData)[M]) {
+  int iRow = 0;
+  int iCol = 0;
+
+  for( iRow=0; iRow<M; ++iRow)
+    for( iCol=0; iCol<M; ++iCol)
+      printf("%d ", piData[iRow][iCol]);
+}
+
+int main( int argc, char *argv[])
+{
+  // Create an 2D array
+  int aiData[][M] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+  //Pass array as a parameter
+  ReadArray(aiData);
+
+  return 0;
+}
