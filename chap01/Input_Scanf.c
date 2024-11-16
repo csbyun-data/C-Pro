7c4da5aa5f7f60c7bbd47bcbe72b7ad11e3c0c35
csbyun-data <time_series@naver.com>
2024-11-16 13:05:47 +0900

    Update Input_Scanf.c

--- chap01/Input_Scanf.c
+++ chap01/Input_Scanf.c
@@ -3,17 +3,18 @@
 
 int main()
 {
-	int age;
-	char name[10];
-	char address[10];
+    int age;
+    
+    char name[10];
+    char address[10];
 	
-	printf("input name, age : ");
-	scanf("%s %d", name, &age);
+    printf("input name, age : ");
+    scanf("%s %d", name, &age);
 	
-	printf("input address. :");
-	scanf("%s", address);  //문자열에 공백이 있는경우 잘림
+    printf("input address. :");
+    scanf("%s", address);  //문자열에 공백이 있는경우 잘림
 	
-	printf("name: %s, age: %d, address:%s \n", name, age, address);
+    printf("name: %s, age: %d, address:%s \n", name, age, address);
 	
-	return 0;
+    return 0;
 }
