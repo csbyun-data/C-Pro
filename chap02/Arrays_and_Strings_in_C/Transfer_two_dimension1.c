e049f543ea5f78819ee60777f9bd6687014c2ea3
csbyun-data <time_series@naver.com>
2024-11-17 00:19:23 +0900

    Update Transfer_two_dimension1.c

--- chap02/Arrays_and_Strings_in_C/Transfer_two_dimension1.c
+++ chap02/Arrays_and_Strings_in_C/Transfer_two_dimension1.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-void ReadArray(int iRow, int iCol, int aiData[][n]) {
+#define ARRAY_COL 3
+
+void ReadArray(int iRow, int iCol, int aiData[][iCol]) {
   int i, j;
 
-  for( i=0; i<ARRAY_ROW; ++i)
-    for( j=0; j<ARRAY_COL; ++j)
+  for( i=0; i<iRow; ++i)
+    for( j=0; j<iCol; ++j)
       printf("%d ", aiData[i][j]);
 }
 
@@ -13,7 +15,7 @@ int main( int argc, char *argv[])
   // Create an 2D array
   int aiData[][ARRAY_COL] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
 
-  int iRow=3, iCol=3;
+  int iRow=3, iCol=ARRAY_COL;
   //Pass array as a parameter
   ReadArray(iRow, iCol, aiData);
 
