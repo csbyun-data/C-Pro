caf98c04129e49686f81e0a74a5e0b57c3a81a9c
csbyun-data <time_series@naver.com>
2024-11-16 17:16:00 +0900

    Create Flow_control4.c

---
+++ chap01/Flow_control4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int i = 1;
+	while(i <= 10)
+	{
+	    if( i == 5) {
+	    	i++;
+			continue;
+		}
+	    printf("%d", i);    //1, 2, 3, 4, 6, 7 출력
+	    if( i == 7) break;
+	    i++;
+	}
+    return 0;
+}
