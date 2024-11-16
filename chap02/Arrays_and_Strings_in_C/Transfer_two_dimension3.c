4afe2f81c7f817171d5bf1a513b7157a503df3ac
csbyun-data <time_series@naver.com>
2024-11-17 00:31:59 +0900

    Update Transfer_two_dimension3.c

--- chap02/Arrays_and_Strings_in_C/Transfer_two_dimension3.c
+++ chap02/Arrays_and_Strings_in_C/Transfer_two_dimension3.c
@@ -3,19 +3,22 @@
 void ReadArray(int *piData, int iRow, int iCol) {
   int i, j;
 
-  for( i=0; i<iRow; ++i)
-    for( j=0; j<iCol; ++j)
-      printf("%d ", aiData[i][j]);
+  for( i=0; i<iRow; i++)
+    for( j=0; j<iCol; j++)
+      printf("%d ", *((piData+i*iRow)+j) );
 }
 
 int main( int argc, char *argv[])
 {
   // Create an 2D array
-  int aiData[][ARRAY_COL] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+  int aiData[][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
 
-  int iRow=3, iCol=ARRAY_COL;
+  int iRow=3, iCol=3;
+
+  // We can also use "ReadArray( &aiData, iRow, iCol);"
+  // and "ReadArray( (int *)aiData[0], iRow, iCol);"
   //Pass array as a parameter
-  ReadArray(iRow, iCol, aiData);
+  ReadArray((int *)aiData, iRow, iCol);
 
   return 0;
 }
