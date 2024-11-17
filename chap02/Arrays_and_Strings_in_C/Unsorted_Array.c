8600706e4dfde9702fc4da9b33279be8af800b69
csbyun-data <time_series@naver.com>
2024-11-17 17:55:02 +0900

    Create Unsorted_Array.c

---
+++ chap02/Arrays_and_Strings_in_C/Unsorted_Array.c
@@ -0,0 +1,38 @@
+// creating an unsorted array and analyzing the processing time
+
+#include <stdio.h>
+#include <stdlib.h> // rand()
+#include <time.h>   // time()
+
+int main()
+{
+    // Generate data
+    const unsigned arraySize = 10000;
+    int data[arraySize];
+    for (unsigned c = 0; c < arraySize; ++c)
+    {
+        data[c] = rand() % 256;
+    }
+    
+    // Test timing
+    clock_t start_time, end_time;
+    double execution_time;
+    
+    start_time = clock();
+    long long sum = 0;
+    for (unsigned i = 0; i < 10000; ++i)
+    {
+        // Primary loop
+        for (unsigned c = 0; c < arraySize; ++c)
+        {
+            if (data[c] >= 128)
+                sum += data[c];
+        }
+    }
+    
+    execution_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("execution time : %f \n", execution_time);
+    printf("sum : %ld \n", sum);
+   
+    return 0;
+}
