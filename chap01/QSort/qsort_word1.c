1da06c70c5e067040be2d74edfd9231d0e7e5a95
csbyun-data <time_series@naver.com>
2024-11-19 15:30:07 +0900

    Create qsort_word1.c

---
+++ chap01/QSort/qsort_word1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  char word[10];
+} book;
+
+int compare ( const book *a, const book *b) {
+  return strcmp( a->word, b->word);
+}
+
+int main()
+{
+  book N[10] = { "english", "see", "korea", "hello", "world", "live",
+                   "summit", "pizza", "dog", "cat"};
+  qsort( N, 10, sizeof(book), compare);
+
+  for ( int i=0; i<10; i++) 
+    printf("[%s] ", N[i].word);
+
+  return 0;
+}    
