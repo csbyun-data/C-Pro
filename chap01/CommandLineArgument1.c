b5d45140ae4b6497a2a746bf767620597f8c4b6b
csbyun-data <time_series@naver.com>
2024-11-19 22:07:01 +0900

    Create CommandLineArgument1.c

---
+++ chap01/CommandLineArgument1.c
@@ -0,0 +1,17 @@
+// argc 명령인자의 갯수 인수갯수가 없을때 기본값=1
+// 명령줄 인수를 저장하는 char 포인터 배열 argv[]
+
+// C Program to illustrate the main function with command line arguments
+#include <stdio.h>
+ 
+int main(int argc, char* argv)
+{
+  // printing the coundt of arguments
+  printf("The value of argc is %d\n", argc);
+
+  // prining each argument
+  for (int i = 0; i < argc; i++)
+    printf("%s \n", argv[i]);
+
+  return 0;
+}
