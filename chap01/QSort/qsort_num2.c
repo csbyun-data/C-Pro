3fc6659c1afe58c6932e0f473549d351f1519121
csbyun-data <time_series@naver.com>
2024-11-19 13:00:54 +0900

    Create qsort_num2.c

---
+++ chap01/QSort/qsort_num2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare( const void *a, const void *b) {
+  return *(const int*)a - *(const int*)b;
+}
+
+int *solution( int numbers[], int numbers_len) {
+  int *answer;
+
+  qsort( numbers, numbers_len, sizeif(int), compare);
+
+  answer = numbers;
+  return answer;
+}
+
+int main() {
+  int numbers[] = { 7, 3, 4, 1, 2, 5, 6};
+  int numbers_len = 7;
+  int *ret = solution(numbers, numbers_len);
+
+  for( int i=0; i < numbers_len; i++) {
+    if( i != 0)
+      printf(", ");
+    printf("%d", ret[i]);
+  }
+}
