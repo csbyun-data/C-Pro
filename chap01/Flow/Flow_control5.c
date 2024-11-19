9acb7b1d7184c505a9d8c25122ae8fdb84b780bf
csbyun-data <time_series@naver.com>
2024-11-19 16:13:47 +0900

    Create Flow_control5.c

---
+++ chap01/Flow/Flow_control5.c
@@ -0,0 +1,17 @@
+// Program to add numbers until the user enters zero
+#include <stdio.h>
+
+int main()
+{
+	double num, sum=0;
+	
+	do {
+		printf("Enter a number: ");
+		scanf("%lf", &num);
+		sum += num;
+	} while (num !=0.0);
+	
+	printf("Sum = %2lf", sum);
+	
+	return 0;
+}
