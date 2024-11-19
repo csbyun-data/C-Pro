ff187df496a6d2d42811aa06e017d4208e5ac5d3
csbyun-data <time_series@naver.com>
2024-11-19 16:04:34 +0900

    Create Flow_control1.c

---
+++ chap01/Flow/Flow_control1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int n1, n2;
+	
+  printf("Enter two integers: ");
+  scanf("%d %d", &n1, &n2);
+	
+  if(n1 == n2) {
+    printf("n1 = n2"); 
+  } else if(n1 > n2) {
+    printf("n1 > n2");
+  } else {
+    printf("n1 < n2");
+  }
+	
+	return 0;
+}
