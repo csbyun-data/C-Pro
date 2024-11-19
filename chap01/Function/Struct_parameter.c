7a5895c06cb92f8c27efe7ad2cbd57c0ef60dde1
csbyun-data <time_series@naver.com>
2024-11-19 21:58:04 +0900

    Create Struct_parameter.c

---
+++ chap01/Function/Struct_parameter.c
@@ -0,0 +1,22 @@
+// struct를 parameter로 전달하는 방법
+//call by address 
+#include <stdio.h>
+
+struct Rect {
+	int length;
+	int width;
+};
+
+void changeLength (struct Rect *p, int l) {
+	p->length = l;
+}
+
+int main() 
+{
+	struct Rect r = {10, 5};
+	changeLength(&r, 20);
+	
+	printf("Area = %d", r.length*r.width);
+	
+	return 0;
+}
