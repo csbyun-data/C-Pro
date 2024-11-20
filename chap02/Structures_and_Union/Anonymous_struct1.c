9989ca2ec33978e6c164ca6397a80d8c7683d1b6
csbyun-data <time_series@naver.com>
2024-11-20 13:54:47 +0900

    Create Anonymous_struct1.c

---
+++ chap02/Structures_and_Union/Anonymous_struct1.c
@@ -0,0 +1,21 @@
+//익명의 구조체 사용예 1
+#include <stdio.h>
+#include<string.h>
+struct myData {
+  union {  // anonymous union
+    short int a;
+    short int b;
+  };
+  short int c;
+};
+
+int main()
+{
+  //Variable of structure
+  struct myData sMydata;
+  sMydata.a =10; //valid
+  sMydata.b = 20;  //valid
+  sMydata.c = 40;  //valid
+  
+  return 0;
+}
