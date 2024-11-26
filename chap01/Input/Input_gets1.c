f3b9fb1134af27401130aa553d28e3026fb56fa8
csbyun-data <time_series@naver.com>
2024-11-26 17:59:54 +0900

    Create Input_gets1.c

---
+++ chap01/Input/Input_gets1.c
@@ -0,0 +1,18 @@
+// gets 함수, puts 함수 사용법
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char buffer[100];
+	
+    // 문자열을 받아 buffer에 저장, 공백을 받아들임
+    printf("input String : ");
+    gets(buffer);  
+	
+    // buffer에 저장된 문자열을 출력
+    printf("String : ");
+    puts(buffer); 
+	
+    return 0;
+}
