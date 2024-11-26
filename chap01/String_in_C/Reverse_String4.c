44d2c131ac46ab2fec46aceb1f0c44ab7e74e921
csbyun-data <time_series@naver.com>
2024-11-26 14:59:02 +0900

    Create Reverse_String4.c

---
+++ chap01/String_in_C/Reverse_String4.c
@@ -0,0 +1,13 @@
+// C program to reverse a string using strrev()
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char str[] = "Hello World";
+
+  // reversing string using strrev()
+  printf("Reversed String: %s", strrev(str));
+
+  return 0;
+}
