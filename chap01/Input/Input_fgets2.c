4318e8a9c4617d9e48d596891f31a89159d120bf
csbyun-data <time_series@naver.com>
2024-11-26 19:58:42 +0900

    Update Input_fgets2.c

--- chap01/Input/Input_fgets2.c
+++ chap01/Input/Input_fgets2.c
@@ -9,7 +9,8 @@ int main()
   printf("enter a string : ");
   fgets(string, 256, stdin);  // stdin 표준입력 stream 
 	
-  // newline character('\n')를 제거하고 문자열 끝에 null terminator '\0' 입력 
+  // newline character('\n')를 제거하고 문자열 끝에
+  // null terminator '\0' 입력 
   for(int i=0; i<256; i++) {
 	  if(string[i] == '\n') {
 	    string[i] = '\0';
