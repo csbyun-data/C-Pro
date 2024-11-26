4130f963f89346d6f512a2912bd264668a29bc26
csbyun-data <time_series@naver.com>
2024-11-26 13:18:50 +0900

    Update String_strdup1.c

--- chap01/String_in_C/String_strdup1.c
+++ chap01/String_in_C/String_strdup1.c
@@ -9,17 +9,17 @@ int main(int argc, char *argv[])
 {
 	char *ap = "ABCD";
 	char *bp;
-		
-//  bp = (char*)malloc(strlen(ap)+1); strcpy(bp, ap); 
+
+	// bp = (char*)malloc(strlen(ap)+1); strcpy(bp, ap); 
 	bp = strdup(ap);
-	
+
 	puts(ap);
 	puts(bp);
-	
+
 	printf("ap = %u, bp=%u \n", ap, bp);
-	
+
 	// strdup로 문자열을 복사한 경우 메모리 free를 해야함 
 	free(bp);
-	
+
 	return 0;
 }
