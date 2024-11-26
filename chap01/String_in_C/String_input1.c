2fa14d17fa324637ea867be97e95521288f9773b
csbyun-data <time_series@naver.com>
2024-11-26 15:17:08 +0900

    Update String_input1.c

--- chap01/String_in_C/String_input1.c
+++ chap01/String_in_C/String_input1.c
@@ -6,11 +6,12 @@ int main()
   // declaring string
   char str[50];
 
+  printf("Enter String: ");
   // reading string
   scanf("%s", str);
 
   // print string
-  printf("%s", str);
+  printf("%s", str); // str == &str[0]
 
   return 0;
 }
