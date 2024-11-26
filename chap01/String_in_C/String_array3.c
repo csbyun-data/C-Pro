bb1da02ae466e6d6c7cdb7182aa9283c016e5eb0
csbyun-data <time_series@naver.com>
2024-11-26 13:08:43 +0900

    Create String_array3.c

---
+++ chap01/String_in_C/String_array3.c
@@ -0,0 +1,26 @@
+// 포인터 변수와 문자열의 활용 예
+#include <stdio.h>
+
+int main()
+{
+	char arr[] = "ABCDEFG";
+	char* ptr = arr;
+	int i;
+	
+	printf("arr = %s \n", arr);
+	printf("ptr = %s \n", ptr);
+	
+	for(i=0; i<strlen(ptr); i++) {
+		putchar(*(ptr+i));
+	}
+	putchar('\n');
+	
+	for(; *ptr; ptr++) {
+		putchar(*ptr);
+	}
+	putchar('\n');
+	
+	ptr = arr;
+
+	return 0;
+}
