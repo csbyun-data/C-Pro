85335d64f6624247085daa92595dbe4701139af9
csbyun-data <time_series@naver.com>
2024-11-28 23:35:09 +0900

    Create Parameter_1D_1_2.c

---
+++ chap01/Function/Parameter_1D_1_2.c
@@ -0,0 +1,31 @@
+// Return multiple values from a function in C using the pointer
+// 단일 변수의 call by address (pass by address) 활용
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// function to get user info
+void Getinfo( int *age, float *height, char *name) {
+  printf("\n\n Enter User age = ");
+  scanf("%d",age);
+  printf("\n\n Enter User height = ");
+  scanf("%f",height);
+  printf("\n\n Enter User name = ");
+  scanf("%s",name);
+}
+
+int main()
+{
+  // variable to store age, height, name
+  int age=0;
+  float height=0.0f;
+  char name[24] = {0};
+
+  // Get user info
+  Getinfo( &age, &height, name);
+  printf("\n User age = %d", age);
+  printf("\n User height = %d", height);
+  printf("\n User name = %s", name);
+
+  return 0;
+}
