3076cd96f920bbf0d29fdc7e63329a3b573a6120
csbyun-data <time_series@naver.com>
2024-11-29 20:06:28 +0900

    Create Return_Struct_Ptr1.c

---
+++ chap01/Function/Return_Struct_Ptr1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct _point {
+  int x;
+  int y;
+};
+
+struct _point * func() {
+  static struct _point s = {10, 10};
+
+  return &s;
+}
+
+int main() {
+  struct _point *ps;
+	
+  ps = func();
+  printf("%d, %d\n", ps->x, ps->y);
+}
+
+// 10, 10
