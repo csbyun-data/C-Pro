db045dc10f2f67e3b62f30e785470730334a12a8
csbyun-data <time_series@naver.com>
2024-11-29 17:52:41 +0900

    Create Return_Ptr_Char1.c

---
+++ chap01/Function/Return_Ptr_Char1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* sol(char* names[], int num) {
+	return names[num];
+}
+
+int main() {
+ char* names[5] = {"lukas", "james", "levi", "eli", "max"};
+ int num = 1;
+ 
+ char* ret = sol(names, num);
+ 
+ printf("ret values is %s . \n", ret);
+}
+//결과 : ret values is james .
+// ** Error와 해결 방법
