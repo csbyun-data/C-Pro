a3a3fc3a4d26fcd957788b58dd1138cb1c2ffc7a
csbyun-data <time_series@naver.com>
2024-11-29 20:46:41 +0900

    Update Return_Ptr_int1.c

--- chap01/Function/Return_Ptr_int1.c
+++ chap01/Function/Return_Ptr_int1.c
@@ -9,6 +9,7 @@ int* solution(int arrA[], int arrA_len, int arrB[], int arrB_len) {
   int arrA_idx = 0, arrB_idx = 0;
   int* answer = (int*)malloc(sizeof(int)*(arrA_len + arrB_len));
   int answer_idx = 0;
+  
   while(arrA_idx < arrA_len && arrB_idx < arrB_len){
     if(arrA[arrA_idx] < arrB[arrB_idx])
       answer[answer_idx++] = arrA[arrA_idx++];
@@ -17,6 +18,7 @@ int* solution(int arrA[], int arrA_len, int arrB[], int arrB_len) {
   }
   while(arrA_idx < arrA_len)
     answer[answer_idx++] = arrA[arrA_idx++];
+  
   while(arrB_idx < arrB_len)
     answer[answer_idx++] = arrB[arrB_idx++];
     
