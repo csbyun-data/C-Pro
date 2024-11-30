95a10a909a79c96e57870ab2923aa66e7bf1008b
csbyun-data <time_series@naver.com>
2024-12-01 00:19:36 +0900

    Create Unsorted_Array_Search.c

---
+++ chap01/Array/Unsorted_Array_Search.c
@@ -0,0 +1,36 @@
+// 정렬되지 않은 배열 검색
+// C Program to implement linear search in unsorted array
+#include <stdio.h>
+
+// Function to implement search operation
+int findElement( int arr[], int n, int key) {
+  int i;
+  for ( i=0; i<n; i++)
+    if( arr[i] == key)
+      return i;
+
+  // If the key is not found
+  return -1;
+}
+
+int main()
+{
+  int arr[] = { 12, 34, 10, 6, 40 };
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  // Using a last element as search element
+  int key=40;
+
+  // Function call
+  int position = findElement( arr, n, key);
+
+  if( position == -1)
+    printf("Element not found");
+  else
+    printf("Element found as position: %d", position+1);
+
+  return 0;
+}
+
+
+    
