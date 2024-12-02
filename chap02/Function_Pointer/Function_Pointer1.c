9d3a99429526ce52d1fce7e47ac3ce31ce048c78
csbyun-data <time_series@naver.com>
2024-12-02 11:12:39 +0900

    Create Function_Pointer1.c

---
+++ chap02/Function_Pointer/Function_Pointer1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void func();
+int rect(int);
+
+int main()
+{
+  void (*fptr1)();
+  int (*fptr2)(int);
+  int len, area;
+
+  fptr1 = &func;
+  (*fptr1)();
+
+  fptr2 = &rect;
+  len=10;
+  area = (*fptr2)(len);
+
+  printf("Area = %d\n", area);
+
+  return 0;
+}
+void func() {
+  printf("call func()\n");
+}
+
+int rect(int len) {
+  int area = (len*len);
+
+  return area;
+}
+
+// call func()
+// Area = 100
+  
