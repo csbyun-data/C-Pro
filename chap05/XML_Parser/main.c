a72169866852a72b78e0a32e04ad9d651c624fff
csbyun-data <time_series@naver.com>
2024-12-06 23:35:50 +0900

    Update main.c

--- chap05/XML_Parser/main.c
+++ chap05/XML_Parser/main.c
@@ -8,14 +8,13 @@
 
 int main(int argc, char *argv[]) 
 {
+  XMLDocument doc;
+  if (XMLDocument_load(&doc, "test.xml")) {
+    XMLNode* str = XMLNode_child(doc.root, 0);
+    printf("Struct: %s\n", XMLNode_attr_val(str, (char *)"name"));
 
-	XMLDocument doc;
-	if (XMLDocument_load(&doc, "test.xml")) {
-		XMLNode* str = XMLNode_child(doc.root, 0);
-		printf("Struct: %s\n", XMLNode_attr_val(str, (char *)"name"));
-
-		XMLNodeList* fields = XMLNode_children(str, "field");
-		for (int i = 0; i < fields->size; i++) {
+    XMLNodeList* fields = XMLNode_children(str, "field");
+    for (int i = 0; i < fields->size; i++) {
 			XMLNode* field = XMLNodeList_at(fields, i);
 			XMLAttribute* type = XMLNode_attr(field, "type");
 			type->value = "";
