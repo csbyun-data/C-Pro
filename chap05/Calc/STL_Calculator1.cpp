6b0b1cd8b34285e3105758b2b0c7190574da92d4
csbyun-data <time_series@naver.com>
2024-12-11 16:46:45 +0900

    Create STL_Calculator1.cpp

---
+++ chap05/Calc/STL_Calculator1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+ 
+//A simple calculator using templates
+ 
+template <class T>
+T add(T a, T b) {
+  T result;
+  result = a + b;
+  return result;
+}
+ 
+template <class T>
+T subtract(T a, T b) {
+  T result;
+  result = a - b;
+  return result;
+}
+ 
+template <class T>
+T divide(T a, T b) {
+  T result;
+  result = a / b;
+  return result;
+}
+ 
+template <class T>
+T multiply(T a, T b) {
+  T result;
+  result = a * b;
+  return result;
+}
+ 
+int main(void)
+{
+   
+  std::cout<<add<int>(10,20)<<std::endl;
+  std::cout<<subtract<float>(5.4,2.3)<<std::endl;
+  std::cout<<multiply<int>(4,5)<<std::endl;
+  std::cout<<divide<int>(9,3)<<std::endl;
+  std::cin.get();
+   
+  return 0;
+}  
