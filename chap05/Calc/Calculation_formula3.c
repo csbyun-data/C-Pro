097e1a06a76f65ba48ee374ff3e8c2e6d2839d89
csbyun-data <time_series@naver.com>
2024-12-11 16:10:37 +0900

    Create Calculation_formula3.c

---
+++ chap05/Calc/Calculation_formula3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int plus(int a, int b) {
+  return a + b;
+}
+
+int minus(int a, int b) {
+  return a - b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+int main()
+{
+	int (*fptr[3])(int, int) = {plus, minus, multiply};
+	int i=0;
+	int a=5;
+	int b=3;
+	
+	for(i=0; i<=2; i++) {
+		printf("fptr[%d]= %d \n", i, (*fptr[i])(a, b));
+	}
+
+	return 0;
+}
+
+/*
+fptr[0]= 8
+fptr[1]= 2
+fptr[2]= 15
+*/
