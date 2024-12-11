55639a2e79dc6a4c4a8858cebbcef0280bbb9581
csbyun-data <time_series@naver.com>
2024-12-11 16:38:16 +0900

    Create Calculation_formula4.c

---
+++ chap05/Calc/Calculation_formula4.c
@@ -0,0 +1,44 @@
+// C언어 함수포인터 활용예제
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct Calculator {
+  int (*plus)(int, int);
+  int (*minus)(int, int);
+} Calculator;
+
+int plus(int a, int b) {
+  return a + b;
+}
+
+int minus(int a, int b) {
+  return a - b;
+}
+
+int* solution(int a, int b) {
+  int *answer = (int *) malloc(sizeof(int) * 2);
+  struct Calculator c;
+
+  c.plus = plus;
+  c.minus = minus;
+
+  answer[0] = c.plus(a, b);
+  answer[1] = c.minus(a, b);
+    
+  return answer;
+}
+
+int main() {
+  int a = 3;
+  int b = 5;
+  int *ret = solution(3, 5);
+
+  printf("Ret Value:  {%d, %d} .\n", ret[0], ret[1]);
+
+  free(ret);
+}
+
+/*
+Ret Value: {8, -2} .
+*/
