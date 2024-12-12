f707a7ac59aa4e827e2a5bc9d639795990af9a7e
csbyun-data <time_series@naver.com>
2024-12-12 10:54:09 +0900

    Create Binary_File1.c

---
+++ chap03/File/Binary_File1.c
@@ -0,0 +1,23 @@
+// size_t fread(void *ptr, size_t size_of_elements, size_t number_of_elements, FILE *a_file)
+// size_t fwirte(const void *ptr, size_t size_of_elements, size_t numbeR_of_elements,
+// FILE *a_file)
+
+#include <stdio.h>
+
+int main()
+{
+	FILE *fp;
+	
+	char x[10] = "ABCDEFGHI";
+	
+	fp = fopen("test.bin", "wb");
+	
+	if(fp != NULL) {
+		fwrite(x, sizeof(x[0]), sizeof(x)/sizeof(x[0]), fp);
+	} else {
+		printf("Failed file open!\n");
+	}
+	fclose(fp);
+	
+	return 0;
+}
