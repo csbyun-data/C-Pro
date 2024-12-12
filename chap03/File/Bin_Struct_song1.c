a36a468bb5f70d507001ed09a96fee47dd8e05dd
csbyun-data <time_series@naver.com>
2024-12-12 15:44:42 +0900

    Create Bin_Struct_song1.c

---
+++ chap03/File/Bin_Struct_song1.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TMAX 30
+#define SMAX 15
+#define AMAX 40
+
+void main()
+{
+  struct {
+    char title[TMAX];
+    char singer[SMAX];
+    char album[AMAX];
+    char date[AMAX];
+  }library;
+
+  FILE* data, * binarydata;
+
+  if ((data = fopen("song.in", "rt")) == NULL) {
+    fprintf(stderr, "\nSON.IN not found!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((binarydata = fopen("song.dat", "wb")) == NULL) {
+    fprintf(stderr, "\nSONG.DAT can't be created!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (strlen(fgets(library.title, TMAX, data)) > 1) {     
+    *strchr(library.title, '\n') = '\0';
+    fgets(library.singer, SMAX, data);
+    *strchr(library.singer, '\n') = '\0';
+    fgets(library.album, AMAX, data);
+    *strchr(library.album, '\n') = '\0';
+
+    //fscanf(data, "%ld\n", &library.date);
+    fgets(library.date, AMAX, data);
+    *strchr(library.date, '\n') = '\0';
+
+    if (fwrite(&library, sizeof(library), 1, binarydata) != 1) {
+      fprintf(stderr, "\nDisk full!\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  exit(EXIT_SUCCESS);
+}
