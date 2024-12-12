b3971d6e220cdb8411cbffeed710024dc5d157ee
csbyun-data <time_series@naver.com>
2024-12-12 17:46:00 +0900

    Update Stack_infix_postfix2.c

--- chap05/Calc/Stack_infix_postfix2.c
+++ chap05/Calc/Stack_infix_postfix2.c
@@ -18,33 +18,33 @@ typedef char element;
 #define MAX_STR_SIZE   4096
 
 typedef struct {
-	element data[MAX_STACK_SIZE];
-	int top;
+  element data[MAX_STACK_SIZE];
+  int top;
 } StackType;
 
 // 스택 초기화 함수
 void init_stack(StackType* s) {
-	s->top = -1;
-	//s->capacity = 1;
-	//s->data = (element *) malloc(s->capacity * sizeof(element));
+  s->top = -1;
+  //s->capacity = 1;
+  //s->data = (element *) malloc(s->capacity * sizeof(element));
 }
 
 // 공백 상태 검출 함수
 int is_empty(StackType* s) {
-	return (s->top == -1);
+  return (s->top == -1);
 }
 // 포화 상태 검출 함수
 int is_full(StackType* s) {
-	//return (s->top == (s->capacity - 1));
-	return (s->top == (MAX_STACK_SIZE - 1));
+  //return (s->top == (s->capacity - 1));
+  return (s->top == (MAX_STACK_SIZE - 1));
 }
 // 삽입함수
 void push(StackType* s, element item) {
 	if (is_full(s)) {
-		fprintf(stderr, "스택 포화 에러\n");
-		return;
+    fprintf(stderr, "스택 포화 에러\n");
+    return;
 	} else
-		s->data[++(s->top)] = item;
+    s->data[++(s->top)] = item;
 }
 // 삭제함수
 element pop(StackType* s) {
