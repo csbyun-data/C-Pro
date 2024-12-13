25a743d7e2c100f62ca553e24a5723fa9738fe1c
csbyun-data <time_series@naver.com>
2024-12-13 21:20:22 +0900

    Update main2.c

--- chap01/extern/main2.c
+++ chap01/extern/main2.c
@@ -6,23 +6,23 @@ extern void cnt();
 extern void cnt2();
 
 int main(int argc, char *argv[]) {
-	
-	iCnt = 10;
+  iCnt = 10;
 
-	inc();
-	printf("global iCnt = %d\n", iCnt);
-	
-	inc();
-	printf("global iCnt = %d\n", iCnt);
-	printf("\n");
-		
-	inc2();
-	printf("file outside siCnt = %d\n", siCnt);
-	
-	inc2();
-	printf("file outside siCnt = %d\n", siCnt);
-	
-	siCnt++;
-	printf("file outside siCnt = %d\n", siCnt);	
-	return 0;
+  inc();
+  printf("global iCnt = %d\n", iCnt);
+
+  inc();
+  printf("global iCnt = %d\n", iCnt);
+  printf("\n");
+
+  inc2();
+  printf("file outside siCnt = %d\n", siCnt);
+
+  inc2();
+  printf("file outside siCnt = %d\n", siCnt);
+
+  siCnt++;
+  printf("file outside siCnt = %d\n", siCnt);	
+  
+  return 0;
 }
