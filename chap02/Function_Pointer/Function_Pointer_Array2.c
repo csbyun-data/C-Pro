1b7b60f4cb197f7e198d7210c8ba3454879c0a58
csbyun-data <time_series@naver.com>
2024-12-13 23:05:27 +0900

    Update Function_Pointer_Array2.c

--- chap02/Function_Pointer/Function_Pointer_Array2.c
+++ chap02/Function_Pointer/Function_Pointer_Array2.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
-int add( int a, int b) { return a+b; }
-int substr( int a, int b) { return a-b; }
+int plus( int a, int b) { return a+b; }
+int subtract( int a, int b) { return a-b; }
+int multiple( int a, int b) { return a*b; }
+int divide( int a, int b) { return a/b; }
 
 int main()
 {
-  int (*fp[2])( int, int);
+  int (*fp[4])( int, int);
 
-  fp[0] = add;
+  fp[0] = plus;
   fp[1] = subtract;
+  fp[2] = multiple;
+  fp[3] = divide;
 
-  for( int i=0; i<2; i++) {
+  // int (*fp[4])( int, int) = {plus, subtract, multiple, divide };
+
+  for( int i=0; i<4; i++) {
     printf("no: %d, %d \n", i, fp[i](10, 20));
   }
 
   return 0;
 }
 
-// no: 0, 30
-// no: 1, -10
+/*
+no: 0, 30
+no: 1, -10
+no: 2, 200
+no: 3, 0
+*/
