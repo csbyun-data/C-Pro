65590db48d71bd2eaf242594c60bb6c68b529dee
csbyun-data <time_series@naver.com>
2024-12-13 21:06:58 +0900

    Create main.c

---
+++ chap01/extern/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "cnt.h"
+
+extern int iCnt;
+extern void cnt();
+
+int main(int argc, char *argv[]) {
+	
+	iCnt = 10;
+
+	cnt();
+	printf("iCnt = %d\n", iCnt);
+	
+	cnt();
+	printf("iCnt = %d\n", iCnt);
+		
+	return 0;
+}
+
+/*
+iCnt = 11
+iCnt = 12
+*/
