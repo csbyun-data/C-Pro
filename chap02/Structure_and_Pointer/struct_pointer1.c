f3238c56690b4d860e48553070296459dee89419
csbyun-data <time_series@naver.com>
2024-12-18 13:36:34 +0900

    Update struct_pointer1.c

--- chap02/Structure_and_Pointer/struct_pointer1.c
+++ chap02/Structure_and_Pointer/struct_pointer1.c
@@ -3,28 +3,27 @@
 #include <malloc.h>
 
 struct student {
-	char name[10];
-	int  age;
+  char name[10];
+  int  age;
 };
 
 int main() 
 {
-	struct student st;
-	struct student* pst;
-	
-	strcpy(st.name, "ABCD");
-	st.age = 10;
-	
-	pst = (struct student*)malloc(sizeof(struct student));
-	strcpy(pst->name, "BCDE"); 
-	//strcpy((*pst).name, "BCDE");  동일 표현식 
-	pst->age = 12;
-	
-	
-	printf("name = %s\n", pst->name);
-	printf("age = %d\n", pst->age);
-	
-	return 0;
+  struct student st;
+  struct student* pst;
+
+  strcpy(st.name, "ABCD");
+  st.age = 10;
+
+  pst = (struct student*)malloc(sizeof(struct student));
+  strcpy(pst->name, "BCDE"); 
+  //strcpy((*pst).name, "BCDE");  동일 표현식 
+  pst->age = 12;
+
+  printf("name = %s\n", pst->name);
+  printf("age = %d\n", pst->age);
+
+  return 0;
 }
 
 /*
