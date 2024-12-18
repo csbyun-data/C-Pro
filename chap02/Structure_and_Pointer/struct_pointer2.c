8809947ad9dd3d1a82ca9b15a6d965f706c66456
csbyun-data <time_series@naver.com>
2024-12-18 13:43:53 +0900

    Update struct_pointer2.c

--- chap02/Structure_and_Pointer/struct_pointer2.c
+++ chap02/Structure_and_Pointer/struct_pointer2.c
@@ -4,22 +4,22 @@
 #include <stdlib.h>
 
 struct Person {
-	char name[20];
-	int age;
+  char name[20];
+  int age;
 };
 
 int main()
 {
-	struct Person* pa = (struct Person *)malloc(sizeof(struct Person));
-	strcpy(pa->name, "name1");  // (*pa).name 동일표현 
-	pa->age = 20;               // (*pa).age 동일표현 
-	
-	printf("name: %s, age: %d \n", pa->name, pa->age);
-	
-	free(pa);
-	
-	struct Person p1 = {"name2", 25};
-    printf("name: %s, age: %d \n", p1.name, p1.age);
-	
-	return 0; 
+  struct Person* pa = (struct Person *)malloc(sizeof(struct Person));
+  strcpy(pa->name, "name1");  // (*pa).name 동일표현 
+  pa->age = 20;               // (*pa).age 동일표현 
+  
+  printf("name: %s, age: %d \n", pa->name, pa->age);
+  
+  free(pa);
+  
+  struct Person p1 = {"name2", 25};
+  printf("name: %s, age: %d \n", p1.name, p1.age);
+  
+  return 0; 
 }
