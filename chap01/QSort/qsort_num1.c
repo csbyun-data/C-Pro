387cdd07172d5c7996da5043085a3fad8329e167
csbyun-data <time_series@naver.com>
2024-12-23 23:26:46 +0900

    Update qsort_num1.c

--- chap01/QSort/qsort_num1.c
+++ chap01/QSort/qsort_num1.c
@@ -8,6 +8,10 @@ int compare (const int *a, const int *b) {
 int main() {
   int N[10] = { 21, 10, 41, 33, 25, 17, 68, 27, 14, 29 };
 
+  // 배열의 표현식의 다른 예제
+  // int N[] = { 21, 10, 41, 33, 25, 17, 68, 27, 14, 29 };
+  // int size = sizeof N / sizeof *N;  , sizeof(int)*10 / sizeof(int)
+  
   // 배열 주소, 요소 갯수, 요소 크기, 기준 함수
   qsort ( N, 10, sizeof(int), compare);
 
