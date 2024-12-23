4cfb8145ed9612d30a7bbb0575de7ac6f1652ae5
csbyun-data <time_series@naver.com>
2024-12-23 21:21:17 +0900

    Update Input_fgets.c

--- chap01/Input/Input_fgets.c
+++ chap01/Input/Input_fgets.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-    int age;
-    char name[10];
-    char address[10];
-	
-    printf("input name, age : ");
-    scanf("%s %d", name, &age);
-	
-    printf("input address. :");
+  int age;
+  char name[10];
+  char address[50];
 
-    // 문자열이 공백이 있어도 줄 전체를 읽어들임
-    fflush(stdin); // Keyboard Buffer를 비움
-    fgets(address, 50, stdin); // scanf("%s", address); 제거
-	
-    printf("name: %s, age: %d, address:%s \n", name, age, address);
-	
-    return 0;
+  printf("input name, age : ");
+  scanf("%s %d", name, &age);
+
+  printf("input address. :");
+
+  // 문자열이 공백이 있어도 줄 전체를 읽어들임
+  fflush(stdin); // Keyboard Buffer를 비움
+  fgets(address, 50, stdin); // scanf("%s", address); 제거
+
+  printf("name: %s, age: %d, address:%s \n", name, age, address);
+
+  return 0;
 }
