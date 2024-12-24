57e86f258b63c7562c003232a1bc872718f3f36b
csbyun-data <time_series@naver.com>
2024-12-24 23:40:01 +0900

    Update Function_Pointer2_8.cpp

--- chap02/Function_Pointer_Tutorials/Function_Pointer2_8.cpp
+++ chap02/Function_Pointer_Tutorials/Function_Pointer2_8.cpp
@@ -5,11 +5,11 @@ using namespace std;
 class TMyClass {
   public:
     int DoIt(float a, char b, char c) {
-	  cout << "TMyClass::DoIt"<< endl; return a+b+c;
+      cout << "TMyClass::DoIt"<< endl; return a+b+c;
 	};
 	
     int DoMore(float a, char b, char c) {
-	  cout << "TMyClass::DoMore" << endl; return a-b+c;
+  	  cout << "TMyClass::DoMore" << endl; return a-b+c;
 	};
   /* more of TMyClass */
 };
@@ -20,21 +20,21 @@ typedef int (TMyClass::*pt2Member)(float, char, char);
 // illustrate how to work with an array of member function pointers
 void Array_Of_Member_Function_Pointers() {
   cout << endl << "Executing ’Array_Of_Member_Function_Pointers’" << endl;
-
+  
   // define arrays and ini each element to NULL, <funcArr1> and <funcArr2> are
   // arrays with 10 pointers to member functions which return an int and take
   // a float and two char
   // first way using the typedef
   pt2Member funcArr1[10] = {NULL};
-
+  
   // 2nd way of directly defining the array
   int (TMyClass::*funcArr2[10])(float, char, char) = {NULL};
-
+  
   // assign the function’s address - ’DoIt’ and ’DoMore’ are suitable member
   // functions of class TMyClass like defined above in 2.1-4
   funcArr1[0] = funcArr2[1] = &TMyClass::DoIt;
   funcArr1[1] = funcArr2[0] = &TMyClass::DoMore;
-
+  
   /* more assignments */
   // calling a function using an index to address the member function pointer
   // note: an instance of TMyClass is needed to call the member functions
