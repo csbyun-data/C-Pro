e357d55b4ac7a3b9332b979931ca090d0713ab94
csbyun-data <time_series@naver.com>
2024-12-24 14:13:19 +0900

    Create Pointer_Cast.c

---
+++ chap02/Pointer_and_Array/Pointer_Cast.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int n = 0x30313233;
+
+	printf("%x %c \n", *(char*)&n, *(char*)&n);
+	printf("%x %c \n", *((char*)&n+1), *((char*)&n+1));
+	printf("%x %c \n", *((char*)&n+2), *((char*)&n+2));
+	printf("%x %c \n", *((char*)&n+3), *((char*)&n+3));
+
+ return 0;
+}
