36231ba1bea7132b9efb2a15881d61542945e2c6
csbyun-data <time_series@naver.com>
2024-12-24 12:41:35 +0900

    Update Passing_String1.c

--- chap01/String_in_C/Passing_String1.c
+++ chap01/String_in_C/Passing_String1.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 // Passing string to a Function
 void displayString(char str[]);
 
@@ -7,9 +9,11 @@ int main()
   printf("Enter string: ");
   fgets(str, sizeof(str), stdin);
 
+  // str은 문자열의 첫번째 address, 동일 표현 &a[0], func( &a[0]);
   displayString(str); // Passing string to a function
 }
 
+// str[] == *str 표현
 void displayString( char str[]) {
   printf("String Output: ");
   puts(str);
