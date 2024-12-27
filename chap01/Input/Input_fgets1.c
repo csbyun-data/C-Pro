74a79a22c2f710220c71fcd4bfaca5aca235f374
csbyun-data <time_series@naver.com>
2024-12-27 12:32:44 +0900

    Update Input_fgets1.c

--- chap01/Input/Input_fgets1.c
+++ chap01/Input/Input_fgets1.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-    char *ptr;
-	
-    ptr = (char *)malloc(sizeof(char)*50);
-    fputs("input String : ", stdout);  // stdout 표준촐력 stream 
-	
-    if(fgets(ptr, 50, stdin) != NULL)  // stdin 표준입력 stream 
-        fputs(ptr, stdout);            // stderr 표준에러 stream
-        free(ptr);
-	
-    return 0;
+  char *ptr;
+
+  ptr = (char *)malloc(sizeof(char)*50);
+  fputs("input String : ", stdout);  // stdout 표준촐력 stream 
+
+  if(fgets(ptr, 50, stdin) != NULL)  // stdin 표준입력 stream 
+    fputs(ptr, stdout);            // stderr 표준에러 stream
+    free(ptr);
+
+  return 0;
 }
