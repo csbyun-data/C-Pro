f4c47681482c91d605ecf51e785583738d440585
csbyun-data <time_series@naver.com>
2024-12-29 22:36:51 +0900

    Update Parameter_2D_3-2p.c

--- chap01/Function/Parameter_2D_3-2p.c
+++ chap01/Function/Parameter_2D_3-2p.c
@@ -6,24 +6,24 @@ void avg(int (*ptr)[4], int rows, int cols);
 
 int main()
 {
-	int score[3][4] = {{1,2,3},{4,5,6},{7,8,9}};
-	int rows, cols;
-	
-	rows = sizeof(score)/sizeof(score[0]);
-	cols = sizeof(score[0])/sizeof(score[0][0]);
-	avg(score, rows, cols);
-	
-	return 0;
+  int score[3][4] = {{1,2,3},{4,5,6},{7,8,9}};
+  int rows, cols;
+  
+  rows = sizeof(score)/sizeof(score[0]);
+  cols = sizeof(score[0])/sizeof(score[0][0]);
+  avg(score, rows, cols);
+  
+  return 0;
 }
 
 void avg(int (*ptr)[4], int rows, int cols) {
-	int i, j, a, t;
-	
-	for(i=0; i<rows; i++) {
-		a = t = 0;
-		for(j=0; j<cols; j++)
-			t += *(*(ptr+i)+j); // t += *(ptr[i]+j); or t += ptr[i][j];
+  int i, j, a, t;
+  
+  for(i=0; i<rows; i++) {
+    a = t = 0;
+    for(j=0; j<cols; j++)
+      t += *(*(ptr+i)+j); // t += *(ptr[i]+j); or t += ptr[i][j];
     a = t/cols;
-		printf("%d 의 t=%d, a=%d\n", i+1, t, a);
-	}
+    printf("%d 의 t=%d, a=%d\n", i+1, t, a);
+  }
 }
