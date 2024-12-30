9b3bc19e809487a64850040475e7779a7f3a251a
csbyun-data <time_series@naver.com>
2024-12-30 12:38:10 +0900

    Update pointer_array1.c

--- chap01/Array/pointer_array1.c
+++ chap01/Array/pointer_array1.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-  const char *parr[3];  // 포인터 배열, (char *) arr[3];, 배열을 가리키는 포인터 3개
+  // 포인터 배열, (char *) arr[3];, 배열을 가리키는 포인터 3개
+  const char *parr[3];  
   
   parr[0] = "ABCDEF";
   parr[1] = "BCD";
@@ -11,8 +12,9 @@ int main(void)
   
   for ( int i=0; i < 3; i++)
     printf("parr[%d]->%s\n", i, parr[i]);
-  
-  char (*arr)[3];        // 배열 포인터 , tmp1[3]를 가르키는 포인터 1개
+
+  // 배열 포인터 , tmp1[3]를 가르키는 포인터 1개
+  char (*arr)[3]; 
   char tmp1[3] = {'a','b','c'};
   arr = &tmp1;
   
