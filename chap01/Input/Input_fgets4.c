63f3026baa0d8adee1ad3c7856e9e6a3d08761c8
csbyun-data <time_series@naver.com>
2025-01-02 23:39:53 +0900

    Update Input_fgets4.c

--- chap01/Input/Input_fgets4.c
+++ chap01/Input/Input_fgets4.c
@@ -18,6 +18,7 @@ int main(void)
     * 이 엔터키를 없애기 위해서 문자열 크기 - 1 인 곳에 NULL을 넣는다
     */
     buffer[ strlen(buffer)-1 ] = '₩0';
+    
     /* buffer 크기 + 1 만큼 메모리를 할당 받고, 할당 받은 메모리의 첫 번지 값을 p[i] 에 저장한다.
     * 메모리를 할당 받을때 문자열의 크기보다 1개가 더 큰 이유는 NULL 값을 저장하기 위함이다.
     */
@@ -26,6 +27,7 @@ int main(void)
       printf("main(): memory allocation error!₩n");
       exit(-1);
     }
+    
     /* buffer의 문자열을 p[i]가 가리키고 있는 방금 할당 받은 메모리로 복사 한다.
     * p[i]가 가리키고 있는 영역은 메모리의 Heap 영역이다
     */
