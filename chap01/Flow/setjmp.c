830c692ea89a15f2f1409977ea73c43644242520
csbyun-data <time_series@naver.com>
2025-01-08 16:39:33 +0900

    Update setjmp.c

--- chap01/Flow/setjmp.c
+++ chap01/Flow/setjmp.c
@@ -1,4 +1,5 @@
 // longjmp()가 실행되면 setjmp()로 이동함,
+// https://www.tutorialspoint.com/c_standard_library/c_function_longjmp.htm
 #include <stdio.h>
 #include <string.h>
 #include <setjmp.h> // setjmp(), longjmp()
