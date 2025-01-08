c65175b1fcff432c78a992cf33b3fc4d39b9c22b
csbyun-data <time_series@naver.com>
2025-01-08 20:34:09 +0900

    Create xlisp_setjmp.c

---
+++ chap01/Flow/xlisp_setjmp.c
@@ -0,0 +1,51 @@
+// xlisp.c main() 함수 부분
+
+#include <stdio.h>
+#include <string.h> // strcmp()
+#include <stdlib.h> // exit()
+#include <setjmp.h>
+#include <stdbool.h>
+
+jmp_buf jmpbuf;
+
+xlfail(char *err);
+fexit();
+
+int main(int argc, char *argv[])
+{
+	char str[100];
+	
+	while( true) {
+		setjmp(jmpbuf);
+		
+		printf("문자열 입력(종료:exit) : ");
+		fgets(str, sizeof(str), stdin);
+		str[strcspn(str,"\n")] = '\0';
+		
+		if (strcmp(str, "exit") == 0 )
+			fexit();
+		else {
+			// xlread();
+			// xleval();
+			// xlprint();
+			
+			xlfail(str);
+		}
+	}
+	return 0;
+}
+
+/* xlfail - error handling routine */
+xlfail(char *err) {
+    /* print the error message */
+    printf("error: %s\n",err);
+
+    /* restart */
+    longjmp(jmpbuf,1);
+}
+
+/* fexit - get out of xlisp */
+fexit() {
+	printf("프로그램 종료.\n");
+  exit(1);
+}
