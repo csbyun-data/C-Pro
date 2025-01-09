d73a5bb334613f42c26f1e41ea81e59d249d72fb
csbyun-data <time_series@naver.com>
2025-01-09 12:55:20 +0900

    Update switch-case.c

--- chap01/Flow/switch-case.c
+++ chap01/Flow/switch-case.c
@@ -7,26 +7,26 @@ void func2() {;}
 
 int main()
 {
-	int num;
+  int num;
   
-	printf("num : ");
-	scanf("%d", &num);
-	switch( num) {
-	case 1:
-		func1();
-		break;
-	case 2:
-		func2();
-		break;
-	case 3:
-	case 4:
-	case 5:
-		// condition 3, 4, 5 running
-		break;
-	default:
-		break;
-	}
-	getchar();
-	
-	return 0;
+  printf("num : ");
+  scanf("%d", &num);
+  switch( num) {
+  case 1:
+    func1();
+    break;
+  case 2:
+    func2();
+    break;
+  case 3:
+  case 4:
+  case 5:
+    // condition 3, 4, 5 running
+    break;
+  default:
+    break;
+  }
+  getchar();
+  
+  return 0;
 }
