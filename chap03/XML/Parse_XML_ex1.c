287d3de67c99cf52094a223f12238f855a34bb24
csbyun-data <time_series@naver.com>
2025-01-16 20:16:21 +0900

    Update Parse_XML_ex1.c

--- chap03/XML/Parse_XML_ex1.c
+++ chap03/XML/Parse_XML_ex1.c
@@ -2,82 +2,80 @@
 #include <string.h>
 
 int GetXmlTagValue(char *pResBuf, char *pTag, char *pTagValue) {
-    int len=0, pos = 0;
-    char openTag[100] = {0}; //Opening Tag
-    char closeTag[100] = {0};//Closing Tag
-    int posOpenTag=0, posClosingTag=0;
+  int len=0, pos = 0;
+  char openTag[100] = {0}; //Opening Tag
+  char closeTag[100] = {0};//Closing Tag
+  int posOpenTag=0, posClosingTag=0;
 
-    //check enter buffer
-    len = strlen(pResBuf);
-    if (len<=0) {
-        return -1;
-    }
-
-    //Create Opening Tag
-    memset(openTag, 0, sizeof(openTag));
-    strcpy(openTag, "<");
-    strcat(openTag, pTag);
-    strcat(openTag, ">");
-    //Create Closing tag
-    memset(closeTag, 0, sizeof(closeTag));
-    strcpy(closeTag, "</");
-    strcat(closeTag, pTag);
-    strcat(closeTag, ">");
-    //Get len of open and close tag
-    const int lenOpenTag = strlen(openTag);
-    const int lenCloseTag = strlen(closeTag);
-
-    //Get Opening tag position
-    for (pos=0; pos<len; pos++) {
-        if ( !memcmp(openTag,(pResBuf+pos),lenOpenTag)) {
-            posOpenTag = pos;
-            break;
-        }
-    }
-
-    //Get closing tag position
-    for (pos=0; pos<len; pos++) {
-        if ( !memcmp(closeTag,(pResBuf+pos),lenCloseTag) ) {
-            posClosingTag = pos;
-            break;
-        }
-    }
-
-    //get the tag value
-    if ( (posOpenTag !=0) && (posClosingTag !=0) ) {
-        const int lenTagVal = posClosingTag-posOpenTag-lenOpenTag;
-        const char * const pStartPosTagVal = pResBuf+posOpenTag+lenOpenTag;
-        if (lenTagVal) {
-            //Get tag value
-            memcpy(pTagValue,pStartPosTagVal, lenTagVal);
-            if (strlen(pTagValue)) {
-                return 1;
-            }
-        }
-    }
+  //check enter buffer
+  len = strlen(pResBuf);
+  if (len<=0) {
     return -1;
+  }
+
+  //Create Opening Tag
+  memset(openTag, 0, sizeof(openTag));
+  strcpy(openTag, "<");
+  strcat(openTag, pTag);
+  strcat(openTag, ">");
+  
+  //Create Closing tag
+  memset(closeTag, 0, sizeof(closeTag));
+  strcpy(closeTag, "</");
+  strcat(closeTag, pTag);
+  strcat(closeTag, ">");
+  
+  //Get len of open and close tag
+  const int lenOpenTag = strlen(openTag);
+  const int lenCloseTag = strlen(closeTag);
+
+  //Get Opening tag position
+  for (pos=0; pos<len; pos++) {
+    if ( !memcmp(openTag,(pResBuf+pos),lenOpenTag)) {
+      posOpenTag = pos;
+      break;
+    }
+  }
+
+  //Get closing tag position
+  for (pos=0; pos<len; pos++) {
+    if ( !memcmp(closeTag,(pResBuf+pos),lenCloseTag) ) {
+      posClosingTag = pos;
+      break;
+    }
+  }
+
+  //get the tag value
+  if ( (posOpenTag !=0) && (posClosingTag !=0) ) {
+    const int lenTagVal = posClosingTag-posOpenTag-lenOpenTag;
+    const char * const pStartPosTagVal = pResBuf+posOpenTag+lenOpenTag;
+    if (lenTagVal) {
+      //Get tag value
+      memcpy(pTagValue,pStartPosTagVal, lenTagVal);
+      if (strlen(pTagValue)) {
+        return 1;
+      }
+    }
+  }
+  return -1;
 }
 
 int main()
 {
-    //Response message
-    char Response[]="<Response>\
-    <Name>aticleworld.com</Name>\
-    <year>1.5</year>\
-    <BlogType>Embedded C and C++</BlogType>\
-    <Author>amlendra</Author>\
-    </Response>";
+  //Response message
+  char Response[]="<Response>\
+  <Name>aticleworld.com</Name>\
+  <year>1.5</year>\
+  <BlogType>Embedded C and C++</BlogType>\
+  <Author>amlendra</Author>\
+  </Response>";
 
-    //pTag name which value you want to access
-    char pTag[] = "BlogType";
-    //Buffer to store tag value
-    char pTagValue[100]= {0};
+  char pTag[] = "BlogType";    //pTag name which value you want to access
+  char pTagValue[100]= {0};    //Buffer to store tag value
 
-    //Function to get tag value
-    GetXmlTagValue(Response,pTag,pTagValue);
+  GetXmlTagValue(Response,pTag,pTagValue); //Function to get tag value
 
-    //Print pTag Value
-    printf("%s",pTagValue);
+  printf("%s",pTagValue); //Print pTag Value
 
-    return 0;
+  return 0;
 }
