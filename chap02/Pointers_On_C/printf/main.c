1933edcf5dacb7f188dfa10579e362f4d36bf80c
csbyun-data <time_series@naver.com>
2025-01-31 19:58:27 +0900

    Update main.c

--- chap02/Pointers_On_C/printf/main.c
+++ chap02/Pointers_On_C/printf/main.c
@@ -5,16 +5,16 @@
 #include "printf.h"
 
 int main( void ) {
-	char ch = 'X';
-	int val = 1289;
-	double fval = 213.04391;
-	char str[128] = "Hello, world! This is Xiangzhen Sun";
-	print_format( "the character is: %c\n",ch );
-	print_format( "the integer is: %d\n", val );
-	print_format( "the float number is: %f\n", fval );
-	print_format( "the string is: %s\n", str );
-	/* error tests */
-	print_format( "the string is: %k\n", str );
-	print_format( "the string is: ", str );
-	return 0;
+  char ch = 'X';
+  int val = 1289;
+  double fval = 213.04391;
+  char str[128] = "Hello, world! This is Xiangzhen Sun";
+  print_format( "the character is: %c\n",ch );
+  print_format( "the integer is: %d\n", val );
+  print_format( "the float number is: %f\n", fval );
+  print_format( "the string is: %s\n", str );
+  /* error tests */
+  print_format( "the string is: %k\n", str );
+  print_format( "the string is: ", str );
+  return 0;
 }
