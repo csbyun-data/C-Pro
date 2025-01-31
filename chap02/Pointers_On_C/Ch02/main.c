2afdeefd8c053ba2936bde6d0eb860203a43bf90
csbyun-data <time_series@naver.com>
2025-01-31 23:58:39 +0900

    Update main.c

--- chap02/Pointers_On_C/Ch02/main.c
+++ chap02/Pointers_On_C/Ch02/main.c
@@ -4,11 +4,11 @@
 #include "negate.h"
 
 int main( void ) {
-  printf("%d\n", increment( 10 ));
-  printf("%d\n", increment( 0 ));
-  printf("%d\n", increment( -10 ));
-  printf("%d\n", negate( 10 ));
-  printf("%d\n", negate( 0 ));
-  printf("%d\n", negate( -10 ));
+  printf("increment( 10 ) : %d\n", increment( 10 ));
+  printf("increment( 0 ) : %d\n", increment( 0 ));
+  printf("increment( -10 ) : %d\n", increment( -10 ));
+  printf("negate( 10 ) : %d\n", negate( 10 ));
+  printf("negate( 0 ) : %d\n", negate( 0 ));
+  printf("negate( -10 ) : %d\n", negate( -10 ));
   return 0;
 }
