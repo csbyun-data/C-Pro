a8eb4f741ee116e22f909ac339514dcaf84a7f74
csbyun-data <time_series@naver.com>
2025-02-13 15:33:28 +0900

    Create unix2dos.c

---
+++ chap03/Split/unix2dos.c
@@ -0,0 +1,29 @@
+// https://github.com/vonj/snippets.org/blob/master/unix2dos.c
+
+/*
+**  UNIX2DOS.C - Convert Unix-style pathnames to DOS-style
+**
+**  public domain by Bob Stout
+*/
+
+#include "filnames.h"
+
+char *unix2dos(char *path) {
+  char *p;
+
+  for (p = path; *p; ++p)
+    if ('/' == *p)
+      *p = '\\';
+  
+  return path;
+}
+
+char *dos2unix(char *path) {
+  char *p;
+
+  for (p = path; *p; ++p)
+    if ('\\' == *p)
+      *p = '/';
+  
+  return path;
+}
