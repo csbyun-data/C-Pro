d379c405614d088a67a3aa6225235fc8df1b607a
csbyun-data <time_series@naver.com>
2025-02-20 18:46:48 +0900

    Update whicharc.cpp

--- chap03/Arch/whicharc.cpp
+++ chap03/Arch/whicharc.cpp
@@ -1,3 +1,5 @@
+// https://github.com/vonj/snippets.org/blob/master/whicharc.cpp
+
 /*
 ** WhichArc.CPP
 **
@@ -17,7 +19,6 @@
 #include "archive.hpp"
 #include "whicharc.h"
 
-
 /*
 **  Definitions of normal archives, excluding LHA archives
 */
@@ -25,13 +26,13 @@
 #define ARCS (sizeof(Arcs)/sizeof(Arcs[0]))
 
 Archive Arcs[] = {
-      Archive (0,  "PK\x03\x04",    ZIP),
-      Archive (0,  "\x60\xEA",      ARJ),
-      Archive (0,  "ZOO",           ZOO),
-      Archive (0,  "HPAK",          HPK),
-      Archive (0,  "Rar!\x1a\x7\0", RAR),
-      Archive (0,  "UC2\x1a",       UC2),
-      Archive (0,  "HLSQZ",         SQZ)
+  Archive (0,  "PK\x03\x04",    ZIP),
+  Archive (0,  "\x60\xEA",      ARJ),
+  Archive (0,  "ZOO",           ZOO),
+  Archive (0,  "HPAK",          HPK),
+  Archive (0,  "Rar!\x1a\x7\0", RAR),
+  Archive (0,  "UC2\x1a",       UC2),
+  Archive (0,  "HLSQZ",         SQZ)
 };
 
 /*
@@ -41,8 +42,8 @@ Archive Arcs[] = {
 #define LHARCS (sizeof(LhArcs)/sizeof(LhArcs[0]))
 
 LhaArchive LhArcs[] = {
-      LhaArchive (0, "-lz-"),
-      LhaArchive (0, "-lh-")
+  LhaArchive (0, "-lz-"),
+  LhaArchive (0, "-lh-")
 };
 
 /*
@@ -52,29 +53,29 @@ LhaArchive LhArcs[] = {
 #define SFXARCS (sizeof(SfxArcs)/sizeof(SfxArcs[0]))
 
 Archive SfxArcs[] = {
-      Archive (0x31F0, "PK\x03\x04\x0A", ZIP,   SFXZIP),
-      Archive (0x31F0, "PK\x03\x04\x14", ZIP2,  SFXZIP2),
-      Archive (0x3CCB, "PK\x03\x04",     ZIP2,  SFXZIP2),
-      Archive (0x3D9A, "PK\x03\x04",     ZIP2,  SFXZIP2),
-
-      // PkZip 1.02 for OS/2 family mode
-      Archive (0x6ED6, "PK\x03\x04\x0A", ZIP,   SFXZIP),
-
-      // PkZip 1.02 for OS/2 native mode
-      Archive (0x5901, "PK\x03\x04\x0A", ZIP,   SFXZIP),
-
-      Archive (0xBBA,  "PK\x03\x04\x0A", ZIP,   SFXZIP),
-      Archive (0xBBA,  "PK\x03\x04\x14", ZIP2,  SFXZIP2),
-
-      // Zip 2.0.1 for OS/2
-      Archive (0x3CE4, "PK\x03\x04\x14", ZIP2,  SFXZIP2),
-
-      Archive (0x39BA, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.22
-      Archive (0x1511, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.22 sfxjr
-      Archive (0x3A0A, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.30
-      Archive (0x14D1, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.30 sfxjr
-      Archive (0x3B02, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.41
-      Archive (0x4F4,  "\x60\xEA",       ARJ,   SFXARJ)   // arj 2.41 sfxjr
+  Archive (0x31F0, "PK\x03\x04\x0A", ZIP,   SFXZIP),
+  Archive (0x31F0, "PK\x03\x04\x14", ZIP2,  SFXZIP2),
+  Archive (0x3CCB, "PK\x03\x04",     ZIP2,  SFXZIP2),
+  Archive (0x3D9A, "PK\x03\x04",     ZIP2,  SFXZIP2),
+  
+  // PkZip 1.02 for OS/2 family mode
+  Archive (0x6ED6, "PK\x03\x04\x0A", ZIP,   SFXZIP),
+  
+  // PkZip 1.02 for OS/2 native mode
+  Archive (0x5901, "PK\x03\x04\x0A", ZIP,   SFXZIP),
+  
+  Archive (0xBBA,  "PK\x03\x04\x0A", ZIP,   SFXZIP),
+  Archive (0xBBA,  "PK\x03\x04\x14", ZIP2,  SFXZIP2),
+  
+  // Zip 2.0.1 for OS/2
+  Archive (0x3CE4, "PK\x03\x04\x14", ZIP2,  SFXZIP2),
+  
+  Archive (0x39BA, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.22
+  Archive (0x1511, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.22 sfxjr
+  Archive (0x3A0A, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.30
+  Archive (0x14D1, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.30 sfxjr
+  Archive (0x3B02, "\x60\xEA",       ARJ,   SFXARJ),  // arj 2.41
+  Archive (0x4F4,  "\x60\xEA",       ARJ,   SFXARJ)   // arj 2.41 sfxjr
 };
 
 /*
@@ -84,11 +85,11 @@ Archive SfxArcs[] = {
 #define SFXLHARCS (sizeof(SfxLhArcs)/sizeof(SfxLhArcs[0]))
 
 LhaArchive SfxLhArcs[] = {
-      LhaArchive (0x653,  "-lh-"),
-      LhaArchive (0x799,  "-lh-"),
-      LhaArchive (0x665,  "-lh-"),
-      LhaArchive (0x664,  "-lh-"),
-      LhaArchive (0x59B1, "-lh-")   // LH for OS/2 2.22
+  LhaArchive (0x653,  "-lh-"),
+  LhaArchive (0x799,  "-lh-"),
+  LhaArchive (0x665,  "-lh-"),
+  LhaArchive (0x664,  "-lh-"),
+  LhaArchive (0x59B1, "-lh-")   // LH for OS/2 2.22
 };
 
 /*
@@ -98,8 +99,8 @@ LhaArchive SfxLhArcs[] = {
 #define SFXPAKS (sizeof(SfxPaks)/sizeof(SfxPaks[0]))
 
 PakArchive SfxPaks[] = {
-      PakArchive (0x261E),
-      PakArchive (0x1AD3)
+  PakArchive (0x261E),
+  PakArchive (0x1AD3)
 };
 
 
@@ -108,105 +109,91 @@ PakArchive SfxPaks[] = {
 */
 
 #ifdef __cplusplus
-   extern "C" {
+  extern "C" {
 #endif
 
-int SeekFile (FILE *fp, char *header, long pos)
-{
-      memset (header, 0, BUFFSIZE);
-      fseek (fp, pos, SEEK_SET);
-      return fread (header, 1, BUFFSIZE - 1, fp);
+int SeekFile (FILE *fp, char *header, long pos) {
+  memset (header, 0, BUFFSIZE);
+  fseek (fp, pos, SEEK_SET);
+  return fread (header, 1, BUFFSIZE - 1, fp);
 } // SeekFile
 
 
-int WhichArc (char *szFileName)
-{
-      char  header[BUFFSIZE];
+int WhichArc (char *szFileName) {
+  char  header[BUFFSIZE];
 
-      memset (header, 0, sizeof (header));
-      FILE *fp = fopen (szFileName, "rb");
-      if (fp == NULL)
-            return ERROR;                    // error opening file
-      int n = fread (header, 1, BUFFSIZE - 1, fp);
+  memset (header, 0, sizeof (header));
+  FILE *fp = fopen (szFileName, "rb");
+  if (fp == NULL)
+        return ERROR;                    // error opening file
+  int n = fread (header, 1, BUFFSIZE - 1, fp);
 
-      if (n <= 0)                        // error reading from file
-      {
-            fclose (fp);
-            return ERROR;
+  if (n <= 0) {                      // error reading from file
+    fclose (fp);
+    return ERROR;
+  }
+
+  int i, rc;
+
+  if (header[0]=='M' && header[1]=='Z') {  // some sort of .EXE file
+    fseek (fp, 0, SEEK_END);
+    long flen = ftell (fp);
+
+    for (i = 0; i < SFXARCS; i++) {
+      rc = SfxArcs[i].Scan (fp, header, flen);
+      if (rc) {
+        fclose (fp);
+        return rc;
       }
+    }
 
-      int i, rc;
-
-      if (header[0]=='M' && header[1]=='Z')   // some sort of .EXE file
-      {
-            fseek (fp, 0, SEEK_END);
-            long flen = ftell (fp);
-
-            for (i = 0; i < SFXARCS; i++)
-            {
-                  rc = SfxArcs[i].Scan (fp, header, flen);
-                  if (rc)
-                  {
-                        fclose (fp);
-                        return rc;
-                  }
-            }
-
-            for (i = 0; i < SFXLHARCS; i++)
-            {
-                  rc = SfxLhArcs[i].Scan (fp, header, flen);
-                  if (rc)
-                  {
-                        fclose (fp);
-                        return rc;
-                  }
-            }
-
-            for (i = 0; i < SFXPAKS; i++)
-            {
-                  rc = SfxPaks[i].Scan (fp, header, flen);
-                  if (rc)
-                  {
-                        fclose (fp);
-                        return rc;
-                  }
-            }
-
-            fclose (fp);
-            return EXE;
+    for (i = 0; i < SFXLHARCS; i++) {
+      rc = SfxLhArcs[i].Scan (fp, header, flen);
+      if (rc) {
+        fclose (fp);
+        return rc;
       }
+    }
 
-      // if file is not .EXE file, define just general types
-      // for different archives. Offset parameter == 0 because
-      // we do not scan files, just read header from the file start
-
-      for (i = 0; i < ARCS; i++)
-      {
-            rc = Arcs[i].Check (header);
-            if (rc)
-            {
-                  fclose (fp);
-                  return rc;
-            }
+    for (i = 0; i < SFXPAKS; i++) {
+      rc = SfxPaks[i].Scan (fp, header, flen);
+      if (rc) {
+        fclose (fp);
+        return rc;
       }
+    }
 
-      for (i = 0; i < LHARCS; i++)
-      {
-            rc = LhArcs[i].Check (header);
-            if (rc)
-            {
-                  fclose (fp);
-                  return rc;
-            }
-      }
+    fclose (fp);
+    return EXE;
+  }
 
-      PakArchive Pak;
+  // if file is not .EXE file, define just general types
+  // for different archives. Offset parameter == 0 because
+  // we do not scan files, just read header from the file start
 
-      rc = Pak.Check (header);
-
+  for (i = 0; i < ARCS; i++) {
+    rc = Arcs[i].Check (header);
+    if (rc) {
       fclose (fp);
-
       return rc;
+    }
+  }
+
+  for (i = 0; i < LHARCS; i++) {
+    rc = LhArcs[i].Check (header);
+    if (rc) {
+      fclose (fp);
+      return rc;
+    }
+  }
+
+  PakArchive Pak;
+
+  rc = Pak.Check (header);
+
+  fclose (fp);
+
+  return rc;
 }
 
 #ifdef TEST
@@ -240,28 +227,26 @@ char *Arctypes[] = {
 
 int main (int argc, char *argv[])
 {
-      int arc_type;
+  int arc_type;
 
-      if (argc != 2)
-      {
-            printf ("USAGE: WHICHARC arcfile\n");
-            return 100;
-      }
+  if (argc != 2) {
+    printf ("USAGE: WHICHARC arcfile\n");
+    return 100;
+  }
 
-      arc_type = WhichArc (argv[1]);
+  arc_type = WhichArc (argv[1]);
 
-      if (arc_type == -1)
-            printf ("File %s not found!\n", argv[1]);
-      else
-      {
-            printf ("%s:\tArchive type is %s\t(return value = %d)\n",
-                    argv[1],
-                    Arctypes[arc_type],
-                    arc_type
-                   );
-      }
+  if (arc_type == -1)
+    printf ("File %s not found!\n", argv[1]);
+  else {
+    printf ("%s:\tArchive type is %s\t(return value = %d)\n",
+            argv[1],
+            Arctypes[arc_type],
+            arc_type
+           );
+  }
 
-      return arc_type;
+  return arc_type;
 }
 
 #endif
