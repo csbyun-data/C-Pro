ae49e548163ba9f982c9d28f10709b1b8b5b7c5e
csbyun-data <time_series@naver.com>
2025-02-20 19:27:33 +0900

    Update archive.hpp

--- chap03/Arch/archive.hpp
+++ chap03/Arch/archive.hpp
@@ -19,44 +19,40 @@
 // LHA files. Add your new types also in the Arctypes array in the TEST part
 // and give them new descriptions.
 
-typedef enum
-{
-      ERROR = -1, UNKNOWN = 0,
-      ARC,      ARC6,     PAK,     ZOO,     HPK,    RAR,    UC2,    SQZ,
-      ARJ,      LARC,     LHARC,   LHA,     ZIP,    ZIP2,
-      SFXARC,   SFXARC6,  SFXPAK,
-      SFXARJ,   SFXLHARC, SFXLHA,  SFXZIP,  SFXZIP2,
-      EXE,
-      COUNT_ARCTYPES
+typedef enum {
+  ERROR = -1, UNKNOWN = 0,
+  ARC,      ARC6,     PAK,     ZOO,     HPK,    RAR,    UC2,    SQZ,
+  ARJ,      LARC,     LHARC,   LHA,     ZIP,    ZIP2,
+  SFXARC,   SFXARC6,  SFXPAK,
+  SFXARJ,   SFXLHARC, SFXLHA,  SFXZIP,  SFXZIP2,
+  EXE,
+  COUNT_ARCTYPES
 } ARCTYPE;
 
 const int BUFFSIZE = 128;
 
-class Archive
-{
+class Archive {
 protected:
-      int  type, sfxtype;
-      long offset;
-      char *fingerprint;
+  int  type, sfxtype;
+  long offset;
+  char *fingerprint;
 
 public:
-      Archive (long offs, char *fp, int t = UNKNOWN, int t2 = UNKNOWN) :
-                  offset (offs), fingerprint (fp), type(t), sfxtype (t2) { };
+  Archive (long offs, char *fp, int t = UNKNOWN, int t2 = UNKNOWN) :
+              offset (offs), fingerprint (fp), type(t), sfxtype (t2) { };
 
-      int Scan  (FILE *fp, char *szBuff, long flen);
-      virtual int Check (char *szBuff);
+  int Scan  (FILE *fp, char *szBuff, long flen);
+  virtual int Check (char *szBuff);
 };
 
-class LhaArchive : public Archive
-{
+class LhaArchive : public Archive {
 public:
-      LhaArchive (long offs, char *fp) : Archive (offs, fp) { };
-      int Check (char *szBuff);
+  LhaArchive (long offs, char *fp) : Archive (offs, fp) { };
+  int Check (char *szBuff);
 };
 
-class PakArchive : public Archive
-{
+class PakArchive : public Archive {
 public:
-      PakArchive (long offs = 0) : Archive (offs, "") { };
-      int Check (char *szBuff);
+  PakArchive (long offs = 0) : Archive (offs, "") { };
+  int Check (char *szBuff);
 };
