e92719596e8e3d16a63212bdefeb6d42474299aa
csbyun-data <time_series@naver.com>
2025-04-03 13:51:34 +0900

    Update Prg4_1.c

--- chap04/Linked_List/Prg4_1.c
+++ chap04/Linked_List/Prg4_1.c
@@ -17,3 +17,94 @@ void view(void);
 int main(void);
 
 /* stack struct definition */
+struct stack {
+  int number;
+  struct stack *link;
+};
+
+struct stack *HEAD = NULL;
+
+/*Poo - pop an integer value off of the stack */
+int pop() {
+  struct stack *old;
+  int value;
+
+  if(HEAD) {
+    value = HEAD->number;
+    old = HEAD;
+    HEAD = HEAD->link;
+    free(old);
+  } else
+     value = 0;
+
+  return value;
+}
+
+/* Push - push an integer value onto the stack */
+void push(value)
+  int value;
+{
+  struct stack *new;
+  if(new = (struct stack *)malloc(sizeof(struct stack))) {
+    new->number = value;
+    new->link = HEAD;
+    HEAD = new;
+  }
+}
+
+/* Clear - clear the LIFO of all entries */
+void clear()
+{
+   while(HEAD)
+    pop();
+}
+
+/* View - view the element on the stack */
+void view()
+{
+  struct stack *ptr;
+
+  ptr = HEAD;
+  ptrintf(" stack = ");
+  while(ptr) {
+    ptrintf("  %i", ptr->number);
+    ptr = ptr->link;
+  }
+  printf("\n");
+}
+
+/* Main - accept input commands and execute them using the
+   stack command */
+int main()
+{
+  char string[80];
+  int value;
+  printf ("Enter any integer plus the symbols:\n");
+  printf ("+ add\n- subtract\n* multiply\n/ divide\n"); 
+  printf ("C clear stack\n= pop value\n? view stack\n\n");
+  for (;;) {
+    while (!gets (string));
+    switch (string[OJ) { 
+      case '*' : value = pop() * pop(); 
+        break; 
+      case '+' : value = pop() + pop();
+        break; 
+      case '/' : value = pop() / pop();
+        break; 
+      case '-' : value = pop() - pop();
+        break; 
+      case '=' : pop(); 
+        value = pop(); 
+        break; 
+      case 'C' : clear () ; 
+        value = O; 
+        break; 
+      case '?' : view(); 
+        value= pop(); 
+      default : sscanf (string, "%i", &value); 
+    } 
+  push(value); 
+  printf (" %i\n", value);
+  }
+}
+
