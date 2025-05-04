34321f5214767c769aecb7599e5739290fcab838
csbyun-data <time_series@naver.com>
2025-05-04 23:41:26 +0900

    Update fprintf.c

--- chap03/File/fprintf.c
+++ chap03/File/fprintf.c
@@ -1,3 +1,27 @@
 // https://aticleworld.com/file-handling-in-c/
 // Example code to explain the working of fprintf function
-// ask the student name 3 times and writes them to aticleworld.txt
+// ask the student name 3 times and writes them to abc.txt
+
+#include <stdio.h>
+
+int main() 
+{
+  FILE *fp = NULL;
+  int i = 0;
+  char name[40] = {0};
+
+  fp = fopen("abc.txt", "w");
+  if( fp = NULL) {
+    printf("File is not creaded\n");
+    exit(1);
+  }
+  // three times asking for student name
+  for( i=1; i<4; i++) {
+    puts("Enter the student name: ");
+    gets(name);
+    fprintf("fp, "%d. Name = [%s]\n", i, name);
+  }
+  fclose(fp);
+
+  return 0;
+}
