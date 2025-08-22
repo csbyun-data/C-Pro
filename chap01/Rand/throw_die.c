1731f8503b404a44991911401219ae534cf4aa9d
csbyun-data <time_series@naver.com>
2025-08-22 12:49:49 +0900

    Update throw_die.c

--- chap01/Rand/throw_die.c
+++ chap01/Rand/throw_die.c
@@ -18,9 +18,10 @@ int throw_die( void ){
   if( !is_seeded ){ 
   is_seeded = 1; 
   srand( (unsigned int)time( NULL ) ); 
- } 
- do { 
+  } 
+  do { 
     value = rand(); 
- } while( value > MAX_OK_RAND ); 
- return value % 6 + 1; 
+  } while( value > MAX_OK_RAND ); 
+ 
+  return value % 6 + 1; 
 }
